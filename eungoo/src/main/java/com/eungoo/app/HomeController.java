06077d52ea7afd22a7b8ff7d445609f820070612
Eungoo Jung <akasilvernine@gmail.com>
2013-03-19 14:41:02 +0900

    수정 테스트 두번째

--- eungoo/src/main/java/com/eungoo/app/HomeController.java
+++ eungoo/src/main/java/com/eungoo/app/HomeController.java
@@ -6,22 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
-
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/test")
-	public String home(Model model) {
-		logger.info("Git ¼öÁ¤ Å×½ºÆ®");
-		model.addAttribute("message", "Git ¼öÁ¤ Å×½ºÆ®");
+	public String test(Model model) {
+		logger.info("Git ìˆ˜ì • í…ŒìŠ¤íŠ¸");
+		model.addAttribute("message", "Git ìˆ˜ì • í…ŒìŠ¤íŠ¸");
 		return "home";
 	}
-
 }
