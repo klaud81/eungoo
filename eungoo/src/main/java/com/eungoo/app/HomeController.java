073d93de8b2645614a0a8f7a1a5d9cdd73a32003
Eungoo Jung <akasilvernine@gmail.com>
2013-04-23 09:08:02 +0900

    rest Json 추가

--- eungoo/src/main/java/com/eungoo/app/HomeController.java
+++ eungoo/src/main/java/com/eungoo/app/HomeController.java
@@ -1,5 +1,10 @@
 package com.eungoo.app;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,8 +17,19 @@ public class HomeController {
 
 	@RequestMapping(value = "/test")
 	public String test(Model model) {
-		logger.info("Git 수정 테스트");
+		logger.info("URL 테스트");
 		model.addAttribute("message", "Git 수정 테스트");
 		return "home";
+	}
+
+	@RequestMapping(value = "/testJson")
+	public void testJson(Model model) {
+		logger.info("JSON 테스트");
+		List<Map<String, String>> returnList = new ArrayList<Map<String, String>>();
+		Map<String, String> returnMap = new HashMap<String, String>();
+		returnMap.put("name", "정은구");
+		returnMap.put("nick", "SilverNine");
+		returnList.add(returnMap);
+		model.addAttribute("returnList", returnList);
 	}
 }
