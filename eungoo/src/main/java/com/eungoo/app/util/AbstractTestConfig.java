8053163307b5e4b1ab1b8f3b48a831a0e3e87fde
Eungoo Jung <akasilvernine@gmail.com>
2014-01-22 17:34:23 +0900

    Fix

---
+++ eungoo/src/main/java/com/eungoo/app/util/AbstractTestConfig.java
@@ -0,0 +1,35 @@
+package com.eungoo.app.util;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+@ComponentScan(basePackages = "com.eungoo.app", excludeFilters = {
+		@ComponentScan.Filter(Controller.class),
+		@ComponentScan.Filter(Configuration.class) })
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.eungoo.app.repository")
+public class AbstractTestConfig implements TransactionManagementConfigurer {
+
+	@Autowired
+	EntityManagerFactory entityManagerFactory;
+
+	@Bean(name = "transactionManager")
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+		jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+
+		return jpaTransactionManager;
+	}
+
+}
